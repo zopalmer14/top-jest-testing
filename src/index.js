1d185dc70700f15ee14e1b03437ffdf44d09de26
zopalmer14 <zopalmer14@gmail.com>
2024-04-13 12:23:15 -0400

    implement reverseString function and associated tests

--- src/index.js
+++ src/index.js
@@ -12,7 +12,16 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+/* 
+    2. A reverseString function that takes a string 
+    and returns it reversed 
+*/ 
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
 module.exports = {
     sum,
     capitalize,
+    reverseString,
 };
--- src/index.test.js
+++ src/index.test.js
@@ -7,7 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
     expect(index.sum(1, 2)).toBe(3); 
 });
 
-// test for the capitalize() function
+// tests for the capitalize() function
 test('returns the given string with the first letter capitalized', () => {
     // create an array of test cases
     const testCases = [
@@ -22,12 +22,45 @@ test('returns the given string with the first letter capitalized', () => {
         {
             input: 'a',
             expected: 'A'
+        },
+        {
+            input: '',
+            expected: ''
         }
     ];
 
     // check each of the test cases
     testCases.forEach((testCase) => {
         const actual = index.capitalize(testCase.input);
+        expect(actual).toBe(testCase.expected);
+    });
+});
+
+// tests for the reverseString() function
+test('returns the given string reversed', () => {
+    // create an array of test cases
+    const testCases = [
+        {
+            input: 'test string',
+            expected: 'gnirts tset'
+        },
+        {
+            input: 'Houston',
+            expected: 'notsuoH'
+        },
+        {
+            input: 'a',
+            expected: 'a'
+        },
+        {
+            input: '',
+            expected: ''
+        }
+    ];
+
+    // check each of the test cases
+    testCases.forEach((testCase) => {
+        const actual = index.reverseString(testCase.input);
         expect(actual).toBe(testCase.expected);
     });
 });
